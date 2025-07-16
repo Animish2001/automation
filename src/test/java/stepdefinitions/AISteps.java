42bb0413564c67a3d63708a02cd07e1fd28e5d7f
Animish2001 <shrivantanimish@gmail.com>
2025-07-16 17:49:58 +0530

    written code for ai module

--- src/test/java/stepdefinitions/AISteps.java
+++ src/test/java/stepdefinitions/AISteps.java
@@ -1,10 +1,29 @@
 package stepdefinitions;
 
+import io.cucumber.java.en.When;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 import utils.BrowserActions;
+import org.openqa.selenium.*;
 
+import java.time.Duration;
 import java.util.logging.Logger;
 
 public class AISteps extends BrowserActions {
     private static final Logger logger = Logger.getLogger(AISteps.class.getName());
+
+    @When("the user navigates to the AI module")
+    public void ai(){
+//        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10));
+//        WebElement view = wait.until(ExpectedConditions.elementToBeClickable(By.xpath("//span[contains(@class, 'menu-title') and text()='AI']")));
+//        view.click();
+        try {
+            startBrowser();
+        }catch (Exception e){
+
+        }
+    }
 
 }
--- src/test/resources/features/ai.feature
+++ src/test/resources/features/ai.feature
@@ -1,3 +1,6 @@
 Feature: AI module
   @ai_feature
-  Scenario: Successful creation of ai
+  Scenario: Verify AI module loads and displays list of guidebooks
+    When the user navigates to the AI module
+    Then the guidebook list should be displayed
+    And the columns "Name" and "Created On" should be visible
