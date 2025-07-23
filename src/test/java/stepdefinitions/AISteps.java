d45b2b197434b95d0d75f0766b1c2646fe5805fb
Animish2001 <shrivantanimish@gmail.com>
2025-07-23 18:03:34 +0530

    updated ai module

--- src/test/java/stepdefinitions/AISteps.java
+++ src/test/java/stepdefinitions/AISteps.java
@@ -1,6 +1,7 @@
 package stepdefinitions;
 
 import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
 import locators.ReportPageLocator;
 import org.openqa.selenium.By;
@@ -20,8 +21,8 @@ public class AISteps extends BrowserActions {
     private static final Logger logger = Logger.getLogger(AISteps.class.getName());
     private static final org.slf4j.Logger log = LoggerFactory.getLogger(AISteps.class);
 
-    @Given("user is already logged in")
-    public void userLoggedIn(){
+    @Given("user is already logged in site")
+    public void LoggedIn(){
         try{
             startBrowser();
             logger.info("Browser started and opening url");
@@ -53,9 +54,73 @@ public class AISteps extends BrowserActions {
 //        WebElement view = wait.until(ExpectedConditions.elementToBeClickable(By.xpath("//span[contains(@class, 'menu-title') and text()='AI']")));
 //        view.click();
         try {
-            startBrowser();
+//            startBrowser();
+            WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10));
+            WebElement view = wait.until(ExpectedConditions.elementToBeClickable(By.xpath("//span[contains(@class, 'menu-title') and text()='AI']")));
+            view.click();
+            logger.info("Clicked on ai module");
         }catch (Exception e){
+            logger.info("Issue while Click on ai module: AISteps Class");
+            takeScreenshot("Failed to click on the ai module");
+            Assert.fail("Exception occurred: " + e.getMessage());
+        }
+    }
 
+    @Then("clicks on dropdown")
+    public void clickOnDropdwn(){
+        try{
+            WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10));
+            WebElement drpdwn = wait.until(ExpectedConditions.elementToBeClickable(By.xpath("//button[contains(@class, 'dropdown-toggle') and contains(@class, 'btn-shadow')]/img[@src='assets/icons/Icon_addguidebook_Black_Unfilled.svg']/parent::button")));
+            drpdwn.click();
+            logger.info("Clicked on dropdown");
+        }catch (Exception e){
+            logger.info("Issue while CLick on dropdown: AISteps Class");
+            takeScreenshot("Failed to click on the dropdown");
+            Assert.fail("Exception occurred: " + e.getMessage());
+
+        }
+    }
+
+    @Then("clicks on Create New")
+    public void clickOnCreateNew(){
+        try{
+            WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10));
+            WebElement createnew = wait.until(ExpectedConditions.elementToBeClickable(By.xpath("//a[contains(@class, 'dropdown-item') and contains(text(), 'Create New')]")));
+            createnew.click();
+            logger.info("Clicked on create new");
+        }catch (Exception e){
+            logger.info("Issue while Click on create new");
+            takeScreenshot("Failed to click on create new");
+            Assert.fail("Exception occurred: " + e.getMessage());
+        }
+    }
+
+    @Then("clicks on guide title")
+    public void clickOnGuideTitle(){
+        try {
+            WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10));
+            WebElement guidetle = wait.until(ExpectedConditions.elementToBeClickable(By.xpath("//input[@formcontrolname='name']")));
+            guidetle.click();
+            guidetle.sendKeys("test");
+            logger.info("Clicked on guide title");
+        }catch (Exception e){
+            logger.info("Issue while CLicking on guide title");
+            takeScreenshot("Failed to click on guide title");
+            Assert.fail("Exception occurred: " + e.getMessage());
+        }
+    }
+
+    @Then("clicks on agreement type")
+    public void clickOnAgreementType(){
+        try {
+            WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10));
+            WebElement agreetype = wait.until(ExpectedConditions.presenceOfElementLocated(By.xpath("//ng-select[@formcontrolname='agreementType']//input[   @aria-autocomplete='list' and    contains(@autocomplete, 'a6a5729c1b71') and    @autocapitalize='off' ]")));
+            agreetype.click();
+            logger.info("Clicked on agreement type");
+        }catch (Exception e){
+            logger.info("Issue while clicking on agreement type");
+            takeScreenshot("Failed to click on agreement type");
+            Assert.fail("Exception occurred: " + e.getMessage());
         }
     }
 
--- src/test/resources/features/ai.feature
+++ src/test/resources/features/ai.feature
@@ -1,7 +1,11 @@
 Feature: AI module
   @ai_feature
   Scenario: Successful creation of ai guidebooks
-    Given user is already logged in
+    Given user is already logged in site
     When the user navigates to the AI module
-    Then the guidebook list should be displayed
-    And the columns "Name" and "Created On" should be visible
+#    Then the guidebook list should be displayed
+#    And the columns "Name" and "Created On" should be visible
+    Then clicks on dropdown
+    Then clicks on Create New
+    Then clicks on guide title
+    Then clicks on agreement type
