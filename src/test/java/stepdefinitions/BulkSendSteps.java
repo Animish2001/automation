7432868582f483b9f476be039b36d61676552c13
Animish2001 <shrivantanimish@gmail.com>
2025-08-22 18:01:39 +0530

    bulk send module

--- src/test/java/stepdefinitions/BulkSendSteps.java
+++ src/test/java/stepdefinitions/BulkSendSteps.java
@@ -1,6 +1,18 @@
 package stepdefinitions;
 
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import org.slf4j.LoggerFactory;
 import utils.BrowserActions;
 
+import java.util.logging.Logger;
+
 public class BulkSendSteps extends BrowserActions {
+    private static final Logger logger = Logger.getLogger(BulkSendSteps.class.getName());
+    private static final org.slf4j.Logger log = LoggerFactory.getLogger(BulkSendSteps.class);
+
+    private WebDriverWait wait;
+    public WebDriver getDriver(){return driver;}
+
+
 }
